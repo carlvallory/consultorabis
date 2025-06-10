456367b1160c316ab5f520ec6b379ba36e6c354a
Carlos Vallory <carlvallory@gmail.com>
2025-06-10 00:37:37 -0300

    third commit

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,1 @@
 // src/utils/helpers.js
-export function getAbsoluteUrl(relativePath) {
-    // En un entorno de desarrollo, Astro sirve desde la raíz.
-    // En producción, si tu base path no es '/', necesitarías ajustarlo.
-    return new URL(relativePath, import.meta.env.BASE_URL).href;
-}
